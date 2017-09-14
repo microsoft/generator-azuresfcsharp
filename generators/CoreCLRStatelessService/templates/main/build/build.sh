7de3b82ad55f9385dc77c4f44842bb8341c2bacf
Rahul Patchigolla <rapatchi@microsoft.com>
2017-09-14 11:06:56 +0530

    Made changes in yo for tp11

--- generators/CoreCLRStatelessService/templates/main/build/build.sh
+++ generators/CoreCLRStatelessService/templates/main/build/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR=`dirname $0`
 
-dotnet restore $DIR/../<%= serviceProject %> -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://dotnet.myget.org/F/dotnet-core/api/v3/index.json
+dotnet restore $DIR/../<%= serviceProject %> -r ubuntu.16.04-x64 -s /opt/microsoft/sdk/servicefabric/csharp/packages -s https://api.nuget.org/v3/index.json
 dotnet build $DIR/../<%= serviceProject %> -v normal
 
 CURDIR=`pwd`
 cd `dirname $DIR/../<%= serviceProject %>`
-dotnet publish -o $CURDIR/../<%= codePath %>
+dotnet publish --self-contained -r ubuntu.16.04-x64 -o $CURDIR/../<%= codePath %>
 cd -

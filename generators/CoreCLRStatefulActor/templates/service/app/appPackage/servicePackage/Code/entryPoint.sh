fdcfbc89533a3f3c0d90a9e78d80169758dc53a6
Deepak Jangid <deepakj@microsoft.com>
2018-01-14 20:28:22 -0800

    Add dotnet support for redhat in csharp yo templates.

--- generators/CoreCLRStatefulActor/templates/service/app/appPackage/servicePackage/Code/entryPoint.sh
+++ generators/CoreCLRStatefulActor/templates/service/app/appPackage/servicePackage/Code/entryPoint.sh
@@ -9,5 +9,18 @@ check_errs()
 }
 
 DIR=`dirname $0`
+echo 0x3f > /proc/self/coredump_filter
+
+. /etc/os-release
+linuxDistrib=$ID
+if [ $linuxDistrib = "rhel" ]; then
+  source scl_source enable rh-dotnet20
+  exitCode=$?
+  if [ $exitCode != 0 ]; then
+    echo "Failed: source scl_source enable rh-dotnet20 : ExitCode: $exitCode"
+    exit $exitCode
+  fi
+fi
+
 dotnet $DIR/<%= serviceProjName %>.dll $@
 check_errs $?

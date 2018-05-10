3c6703a805187eb0cedd212595a1b99c0f88c9d1
Arram Saichand <t-arsaic@microsoft.com>
2018-05-10 12:21:35 +0530

    Minor Fix in deploy.sh

--- generators/CoreCLRStatefulService/templates/main/deploy/deploy.sh
+++ generators/CoreCLRStatefulService/templates/main/deploy/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+cd `dirname $0`
 sfctl application upload --path <%= appPackage %> --show-progress
 sfctl application provision --application-type-build-path <%= appPackage %>
 sfctl application create --app-name fabric:/<%= appName %> --app-type <%= appTypeName %> --app-version 1.0.0
+cd -

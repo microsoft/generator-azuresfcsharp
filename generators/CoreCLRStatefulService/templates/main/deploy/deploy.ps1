05497e916e614164ea3fb581a4f96bdbaa465a32
Arram Saichand <t-arsaic@microsoft.com>
2018-05-17 21:22:42 +0530

    removed unnecessary comments

--- generators/CoreCLRStatefulService/templates/main/deploy/deploy.ps1
+++ generators/CoreCLRStatefulService/templates/main/deploy/deploy.ps1
@@ -1,9 +1,4 @@
 $AppPath = "$PSScriptRoot\<%= appPackage %>"
-#$sdkInstallPath = (Get-ItemProperty 'HKLM:\Software\Microsoft\Service Fabric SDK').FabricSDKInstallPath
-#$sfSdkPsModulePath = $sdkInstallPath + "Tools\PSModule\ServiceFabricSDK"
-#Import-Module $sfSdkPsModulePath\ServiceFabricSDK.psm1
-
-#Connect-ServiceFabricCluster -ConnectionEndpoint localhost:19000
 Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $AppPath -ApplicationPackagePathInImageStore <%= appPackage %>
 Register-ServiceFabricApplicationType <%= appPackage %>
 New-ServiceFabricApplication fabric:/<%= appName %> <%= appTypeName %> 1.0.0

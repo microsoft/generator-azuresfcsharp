91b566d1c210222201f56a139b12b8439ff0b37c
Rahul <rapatchi@microsoft.com>
2018-06-21 19:47:34 +0530

    Adding show progress to deploy scripts

--- generators/CoreCLRStatelessService/templates/main/deploy/deploy.ps1
+++ generators/CoreCLRStatelessService/templates/main/deploy/deploy.ps1
@@ -1,4 +1,4 @@
 $AppPath = "$PSScriptRoot\<%= appPackage %>"
-Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $AppPath -ApplicationPackagePathInImageStore <%= appPackage %>
+Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $AppPath -ApplicationPackagePathInImageStore <%= appPackage %> -ShowProgress
 Register-ServiceFabricApplicationType <%= appPackage %>
 New-ServiceFabricApplication fabric:/<%= appName %> <%= appTypeName %> 1.0.0
